d0f7feba4e1345537656f741aa11122ea18f0334
Terminatorles <mokoulesedi@gmail.com>
2018-12-05 14:34:06 +0200

    updated the server.js file plus added express in the package.json file

--- server.js
+++ server.js
@@ -1,38 +1,16 @@
+var express = require('express');
+var app = express();
 
-    var http = require('http');
-    var url = require('url');
-    var fs = require('fs');
-    var server = http.createServer(function(request, response) {
-        var path = url.parse(request.url).pathname;
-        switch (path) {
-            case '/':
-                response.writeHead(200, {
-                    'Content-Type': 'text/plain'
-                });
-                response.write("This is Test Message.");
-                response.end();
-                break;
-            case '/index.html':  
-                fs.readFile(__dirname + path, function(error, data) {
-                    if (error) {
-                        response.writeHead(404);
-                        response.write(error);
-                        response.end();
-                    } else {
-                        response.writeHead(200, {
-                            'Content-Type': 'text/html'
-                        });
-                        response.write(data);
-                        response.end();
-                    }
-                });
-                break;
 
-            default:
-                response.writeHead(404);
-                response.write("opps this doesn't exist - 404");
-                response.end();
-                break;
-        }
-    });
-    server.listen(8082);
+var server;
+
+app.use(express.static(__dirname+ '/'));
+
+app.get('/', function(req, res){
+    res.sendFile('index.html');
+});
+
+
+server = app.listen(2086);
+
+console.log(server is up);
